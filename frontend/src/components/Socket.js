8d32fe136603c4176c4dec2bbf1eb71372c42738
Hanna Murai <annmuray75@gmail.com>
2020-09-13 12:07:57 -0500

    changes

--- frontend/src/components/Socket.js
+++ frontend/src/components/Socket.js
@@ -12,74 +12,6 @@ import { Button, Input } from "reactstrap";
 const ENDPOINT = "http://127.0.0.1:4000";
 const socket = socketIOClient(ENDPOINT);
 function Socket(props) {
-  //   const [state, setState] = useState({
-  //     message: "",
-  //     name: props.user.user.username,
-  //     users: []
-  //   });
-  //   const [chat, setChat] = useState([]);
-
-  //   useEffect(() => {
-  //     socket.on("message", ({ name, message }) => {
-  //       setChat([...chat, { name, message }]);
-  //     });
-  //   });
-
-  //   const onTextChange = e => {
-  //     setState({ ...state, [e.target.name]: e.target.value });
-  //   };
-
-  //   const onMessageSubmit = e => {
-  //     e.preventDefault();
-  //     const { name, message, users } = state;
-  //     socket.emit("message", { name, message });
-  //     users.push(name);
-  //     console.log("users", name);
-  //     setState({ message: "", name });
-  //   };
-
-  //   const renderChat = () => {
-  //     console.log("Name", props.user.user.username);
-  //     return chat.map(({ name, message }, index) => (
-  //       <div key={index}>
-  //         <h3>
-  //           {name}: <span>{message}</span>
-  //         </h3>
-  //       </div>
-  //     ));
-  //   };
-
-  //   return (
-  //     <div>
-  //       <Navbar />
-  //       {props.user.user.username}
-  //       <div className="card">
-  //         <form onSubmit={onMessageSubmit}>
-  //           <h1>Messanger</h1>
-  //           <div className="render-chat">
-  //             <h1>Chat Log</h1>
-  //             {renderChat()}
-  //           </div>
-
-  //           <div>
-  //             <h2>Active users</h2>
-  //             <p></p>
-  //           </div>
-  //           <div>
-  //             <textarea
-  //               name="message"
-  //               onChange={e => onTextChange(e)}
-  //               value={state.message}
-  //               variant="outlined"
-  //               label="Message"
-  //             />
-  //           </div>
-  //           <button>Send Message</button>
-  //         </form>
-  //       </div>
-  //     </div>
-  //   );
-
   const [user, setUser] = useState({
     usersList: null
   });
@@ -124,33 +56,35 @@ function Socket(props) {
   return (
     <div>
       <Navbar disconect={disconect} />
-      <h3 className="d-flex justify-content-center">
-        {" "}
-        Connected users : {user.usersList?.length}{" "}
-      </h3>
-      <table className="table">
-        <thead>
-          <tr>
-            <th> User name </th>
-            <th> Connection Date </th>
-          </tr>
-        </thead>
-        <tbody>
-          {user.usersList?.map(user => {
-            return (
-              <tr key={user.id}>
-                <td> {user.userName} </td>
-                <td> {user.connectionTime} </td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
+      <div className="tableNames">
+        <h3 className="d-flex justify-content-center table-primary">
+          {" "}
+          Connected users : {user.usersList?.length}{" "}
+        </h3>
+        <table className="table table-striped">
+          <thead>
+            <tr>
+              <th> User name </th>
+              <th> Connection Date </th>
+            </tr>
+          </thead>
+          <tbody>
+            {user.usersList?.map(user => {
+              return (
+                <tr key={user.id}>
+                  <td> {user.userName} </td>
+                  <td> {user.connectionTime} </td>
+                </tr>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
       <h3 className="d-flex justify-content-center">
         {" "}
         User : {loggedUser?.userName}{" "}
       </h3>
-      <div style={{ borderStyle: "inset" }}>
+      <div>
         <h2 className="d-flex justify-content-center"> Chat </h2>
         {recMsg.listMsg?.map((msgInfo, index) => {
           return (
@@ -184,10 +118,6 @@ function Socket(props) {
           Send{" "}
         </Button>
       </div>
-      {/* <button onClick={() => disconect(props.user.user.username)}>
-        Disconect
-      </button> */}
-      <p></p>
     </div>
   );
 }
